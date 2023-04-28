b6228fa4bc35568dbd28e6f4712d6dc32e072d46
Lance <lanc3.corcega@gmail.com>
2023-04-28 18:14:43 +1200

    Section 18.272(JavaFX; More on Choosers and Web Pages)
    completing file/directoryChoosers, other extensions and multi-select; viewing web-pages through the application (WebView), or system default (popup, Desktop.getDesktop().browse())

--- src/main/java/com/example/javafxapplication_section18javafx/HelloController.java
+++ src/main/java/com/example/javafxapplication_section18javafx/HelloController.java
@@ -5,11 +5,21 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.channels.FileChannel;
+import java.util.List;
+
+import java.awt.Desktop;
+
 
 public class HelloController {
     @FXML
@@ -18,6 +28,9 @@ public class HelloController {
     private Button button4;
     @FXML
     private GridPane gridPane;
+
+    @FXML
+    private WebView webView;
 
     public void initialize() {
         button4.setEffect(new DropShadow());
@@ -37,13 +50,37 @@ public class HelloController {
 
     @FXML
     public void handleClick() {
-//        FileChooser chooser = new FileChooser();
-        DirectoryChooser chooser = new DirectoryChooser();
-        File file = chooser.showDialog(gridPane.getScene().getWindow());
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Save Application File");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Zip", "*.zip"),
+                new FileChooser.ExtensionFilter("PDF", "*.pdf"),
+                new FileChooser.ExtensionFilter("All files", "*.*"),
+                new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png", "*.gif")
+        );
+//        DirectoryChooser chooser = new DirectoryChooser();
+        List<File> file = chooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
         if (file != null) {
-            System.out.println(file.getPath());
+            for(File f : file) {
+                System.out.println(f.getPath());
+            }
         } else {
             System.out.println("Chooser was cancelled");
         }
     }
+
+    @FXML
+    public void handleLinkClick() {
+//        try {
+//            Desktop.getDesktop().browse(new URI("https://www.youtube.com"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (URISyntaxException e) {
+//            e.printStackTrace();
+//        }
+
+        WebEngine engine = webView.getEngine();
+        engine.load("https://youtube.com");
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.javafxapplication_section18javafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.web;
 
 
     opens com.example.javafxapplication_section18javafx to javafx.fxml;
